e62585fe3412d7c1f6d62b91d99490f02198d47b
Mario González <mario@mariogl.com>
2021-07-13 11:04:03 +0200

    Login, logout y carga de datos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [error, setError] = useState(false);
+  const [logueado, setLogueado] = useState(false);
+  const [datos, setDatos] = useState([]);
+  const loguearse = async () => {
+    const credenciales = {
+      usuario: "mariogl",
+      password: "mariogl",
+    };
+    const resp = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credenciales),
+    });
+    if (resp.ok) {
+      setError(false);
+      const { token } = await resp.json();
+      localStorage.setItem("token", token);
+      setLogueado(true);
+    } else {
+      setError(true);
+      setLogueado(false);
+    }
+  };
+
+  const cargarDatos = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return;
+    }
+    const resp = await fetch("http://localhost:4000/datos", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const datos = await resp.json();
+    setDatos(datos.datos);
+  };
+
+  const desloguearse = () => {
+    localStorage.removeItem("token");
+    setLogueado(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!logueado && <button onClick={loguearse}>Login</button>}
+      {error && <p>Error de autentificación</p>}
+      {logueado && <button onClick={desloguearse}>Logout</button>}
+      {logueado && (
+        <>
+          <button onClick={cargarDatos}>Cargar datos</button>
+          <ul>
+            {datos.map((dato) => (
+              <li key={dato}>{dato}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </>
   );
 }
 
